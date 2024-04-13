ba721559734c319817ebb0c61296f4c827a0b4b1
khalil <khalil.benkhaled@esprit.tn>
2024-04-13 13:03:25 +0200

    fix

--- khalil/synthetic_data/prompt.py
+++ khalil/synthetic_data/prompt.py
@@ -17,7 +17,7 @@ def context_relevancy(question: Text, contexts: list[Text]) -> Text:
     bs = '\n'
     return f"""check if the following question can be answered fully from the provided context.
 you should not rely on any prior knowledge you have, rely only on the context.
-your answer should always start with the verdict following this exact format note the number of the {{ and }}: {{verdict:1}} if the context was useful and {{verdict:0}} if it was not, then give me the reasoning behind your verdict
+your answer should always start with the verdict following this exact format note the number of the {{ and }}: {{verdict:1}} if the context was useful and {{verdict:0}} if it was not, then give me the reasoning behind your verdict.
 question: {question}
 context: {bs.join(context for context in contexts)}
 """
@@ -28,10 +28,9 @@ def parse_context_relevency_output(text: Text) -> int:
     """
     parses the judges output that comes out of the prompt context_relevency
     return: 0 or 1 (the veridict  and -1 if there is no verdict)
-you should not rely on any prior knowledge you have, rely only on the context.
-your answer should always start with the verdict following this format: [[verdict:1]] if the context was useful and [[verdict:0]] if it was not, the give me the reasoning behind your verdict
     """
-    match = re.search(r'\{\{verdict:(\d+)\}\}', text)
+    pattern = r'{verdict:(\d)}'
+    match = re.search(pattern, text)
     if match:
         return int(match.group(1))
     else:
