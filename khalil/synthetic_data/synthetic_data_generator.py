6769a2e80dd6f31860c4868df4c81aaf0330955e
khalil <khalil.benkhaled@esprit.tn>
2024-04-21 14:56:03 +0200

    fix

--- khalil/synthetic_data/synthetic_data_generator.py
+++ khalil/synthetic_data/synthetic_data_generator.py
@@ -37,7 +37,10 @@ class Synthetic_data_generator:
     # TODO: Fix the type of the vectordb and the dict
     # TODO: refactor the code below
     # TODO: don't like this
-    def generate_from_vector_db(self,  vector_db, num_questions: int = 3, distribution: dict[str, float] = {'simple': 0.7, 'multiple': 0.3}) -> dict[int, dict[str, str | list[str]]]:
+    def generate_from_vector_db(self,
+                                vector_db,
+                                num_questions: int = 3,
+                                distribution: dict[str, float] = {'simple': 0.7, 'multiple': 0.3}) -> dict[int, dict[str, str | list[str]]]:
         """
         generates synthetic data from an already existing vectordb by follwing the following steps:
         1- gets a random context partially done
@@ -62,43 +65,23 @@ class Synthetic_data_generator:
         collection = self.vector_db.get_collection(
             name="langchain", embedding_function=custom)
         results_all = collection.get()
+
         i: int = 0
-        while (i < simple_question):
-
-            synthetic_data_sample: dict[str, str | list[str]] = {}
-            while not synthetic_data_sample:
-                choice = random.choice(
-                    results_all["documents"]) if results_all["documents"] else None
-
-                # NOTE: THE ASSUMPTION IS THAT THE CONTEXT PROVIDE SIMPLAR BUT SOMEWHAT DIFFERENT INFORMATION/// verify with multiple context quesetion
-                similiar_to_chosen_context = collection.query(
-                    query_texts=[choice] if choice else None,
-                    n_results=3
-                )
-                contexts = similiar_to_chosen_context['documents'][0]
-                # TODO: WHYYYYYYYYYYY
-                print('**************question number', i)
-                synthetic_data_sample = self._generate(contexts, 'simple')
-            synthetic_data = synthetic_data | {i: synthetic_data_sample}
+        while i < simple_question:
+            print('**************generating SIMPLE question number', i)
+            synthetic_data_sample = self._generate(results_all, 'simple')
+            synthetic_data = synthetic_data | {
+                i: synthetic_data_sample}
             i += 1
 
         # TODO: Really don't like the 2 while loops
         i = 0
-        while (i < multiple_context_question):
-
-            choice = random.choice(
-                results_all["documents"]) if results_all["documents"] else None
-
-            # NOTE: THE ASSUMPTION IS THAT THE CONTEXT PROVIDE SIMPLAR BUT SOMEWHAT DIFFERENT INFORMATION/// verify with multiple context quesetion
-            similiar_to_chosen_context = collection.query(
-                query_texts=[choice] if choice else None,
-                n_results=3
-            )
-            contexts = similiar_to_chosen_context['documents'][0]
-            # TODO: WHYYYYYYYYYYY
-            synthetic_data_sample: dict[str, str | list[str]] = self._generate(
-                contexts, 'multiple')
-            synthetic_data = synthetic_data | {i: synthetic_data_sample}
+        while i < simple_question:
+            print('**************generating MULTIPLE question number', i)
+            synthetic_data_sample = self._generate(results_all, 'multiple')
+            synthetic_data = synthetic_data | {
+                i: synthetic_data_sample}
+            i += 1
         return synthetic_data
 
     # TODO: finish the logic for the judge
@@ -106,37 +89,53 @@ class Synthetic_data_generator:
     # TODO: add the POOR feature
     # TODO: check if the dict type works on 3.9>
 
-    def _generate(self, contexts: list[str], type: str) -> dict[str, str | list[str]]:
+    # TODO: deal with the type of results all
+    def _generate(self, results_all, type: str) -> dict[str, str | list[str]]:
         """
         generates one question and verifies it for the given list of contexts
         """
         synthetic_data_sample: dict[str, str | list[str]] = {}
-        question: str = ''
-        not_verified: bool = True
-        # prompts
-        generation_data: dict[str, str | list[str]] = {'contexts': contexts}
-        generation_prompt: Prompt = Prompt(
-            base=TYPES[type],
-            data=generation_data
-        )
-        print('GENERATION PROMPT', generation_prompt.get_text())
+        question: str
+        # TODO: deal with this when dealing with new vectordb
+        collection = self.vector_db.get_collection(
+            name="langchain", embedding_function=custom) if self.vector_db else None
+        MAXIMUM_NUMBER_OF_TRIES: int = 3
+        verdict: int = 0
+        number_of_tries: int = 0
+        while verdict == 0 and number_of_tries < MAXIMUM_NUMBER_OF_TRIES:
+            choice = random.choice(
+                results_all["documents"]) if results_all["documents"] else None
 
-        # TODO: fail safe in case it never gets verified
+            # NOTE: THE ASSUMPTION IS THAT THE CONTEXT PROVIDE SIMPLAR BUT SOMEWHAT DIFFERENT INFORMATION/// verify with multiple context quesetion
+            similiar_to_chosen_context = collection.query(
+                query_texts=[choice] if choice else None,
+                n_results=3
+            ) if collection else None
+            contexts: list[str] = similiar_to_chosen_context['documents'][0] if similiar_to_chosen_context else []
+            # prompts
+            generation_data: dict[str, str | list[str]] = {
+                'contexts': contexts}
+            generation_prompt: Prompt = Prompt(
+                base=TYPES[type],
+                data=generation_data
+            )
+            print('GENERATION PROMPT', generation_prompt.get_text())
 
-        print('*******\nTRYING')
-        question: str = self.generator.generate(
-            generation_prompt.get_text())
-        print('\nCHOSEN QUESTION:', question, '\n')
-        # verify the quality of the question
-        judge_data: dict[str, str | list[str]] = {
-            'question': question,
-            'contexts': contexts
-        }
-        verdict: int = context_relevany_one(self.judge, judge_data)
+            print('*******\nTRYING')
+            question: str = self.generator.generate(
+                generation_prompt.get_text())
+            print('\nCHOSEN QUESTION:', question, '\n')
+            # verify the quality of the question
+            judge_data: dict[str, str | list[str]] = {
+                'question': question,
+                'contexts': contexts
+            }
+            verdict: int = context_relevany_one(self.judge, judge_data)
 
-        if verdict == 1:
-            synthetic_data_sample[question] = contexts
-            not_verified = False
-            return synthetic_data_sample
-        else:
-            return {}
+            if verdict == 1:
+                synthetic_data_sample[question] = contexts
+                return synthetic_data_sample
+            else:
+                number_of_tries += 1
+        print('failing\nexceeded the maximum number of tries without getting to a question')
+        return {}
