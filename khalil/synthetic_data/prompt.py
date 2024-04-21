3319bca7725d0a878c118f1a6c12495cb89c0240
khalil <khalil.benkhaled@esprit.tn>
2024-04-21 15:33:18 +0200

    fix

--- khalil/synthetic_data/prompt.py
+++ khalil/synthetic_data/prompt.py
@@ -3,8 +3,7 @@
 # AFTER REFACTORING
 
 
-SIMPLE_QUESTION_PROMPT: str = """ Generate one and only one question that can be fully answered from the given context,
-your answer should not contain anything other than the question. the question should not contain any information from the context, it also should not be too precise but should be fully answered from the provided context. 
+SIMPLE_QUESTION_PROMPT: str = """ Generate one and only one question that can be fully answered from the given context, assume the person answing this context does not have the context when answring. you answer should only contain the question
 """
 
 MULTIPLE_CONTEXT_QUESTION_PROMPT: str = """ Generate one and only one question that can be fully answered from the given context,
